dd804ac4a0a9fb882153defc1d3a354855e7a0bf
Cesar Augusto Tadeu Oliveira <cesar.oliveira@bernoulli.com.br>
2025-12-10 16:36:53 -0300

    publish only dist folder

--- src/App.tsx
+++
@@ -1,4 +0,0 @@
-import AppRoutes from "./routes";
-export default function App() {
-  return <AppRoutes />;
-}
--- src/middlewares/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import type { RootState } from "../redux/store";
-
-export default function PrivateRoute({ children }: { children: React.ReactElement }) {
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-  return isAuthenticated ? children : <Navigate to="/login" />;
-}
